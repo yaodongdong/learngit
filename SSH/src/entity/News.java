b615b50eab8731846a22f45f48a9a83db6bcd5df
yaodongdong <ydd19921107@gmail.com>
2015-11-25 08:56:02 +0800

    extends commit

---
+++ SSH/src/entity/News.java
@@ -0,0 +1,72 @@
+package entity;
+
+import util.ImageUtil;
+
+
+public class News {
+
+	private String title;
+	private String content;
+	private ImageUtil image;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public ImageUtil getImage() {
+		return image;
+	}
+	public void setImage(ImageUtil image) {
+		this.image = image;
+	}
+	@Override
+	public String toString() {
+		return "News [title=" + title + ", content=" + content + ", image=" + image + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		News other = (News) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ SSH/src/util/ImageUtil.java
@@ -0,0 +1,13 @@
+package util;
+
+import javax.swing.text.Element;
+import javax.swing.text.html.ImageView;
+
+public class ImageUtil extends ImageView {
+
+	public ImageUtil(Element elem) {
+		super(elem);
+		// TODO Auto-generated constructor stub
+	}
+
+}
